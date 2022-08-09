120aafc5eba7320e1073b7e477e0f5c6c9763616
Jishan Kharbanda <jishankharbanda@gmail.com>
2022-08-09 09:02:36 -0700

    Initial Commit

---
+++ College Lists/College_ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  College_ListsApp.swift
+//  College Lists
+//
+//  Created by Jishan Kharbanda on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct College_ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
